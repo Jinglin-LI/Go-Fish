d9f804bffc9a7cff242aa2a5f5b8b15e3746cfcc
Jinglin-LI <lijinglin@mail.nankai.edu.cn>
2018-04-09 22:35:36 -0500

    Go Routine and Channel

--- channels/main.go
+++ channels/main.go
@@ -14,17 +14,27 @@ func main() {
 		"http://amazon.com",
 	}
 
+	// create a channel
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		// Create a new thread go routine and run this function with it
+		go checkLink(link, c)
+	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Might be down I think"
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- "Yep it is up"
 }
